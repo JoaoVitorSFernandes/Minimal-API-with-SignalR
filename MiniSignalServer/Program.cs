f457170b137d5f6d38982750c7fd918a8aca83dd
JoaoVitorSFernandes <joaosilvafernandesdev@gmail.com>
2023-07-10 22:41:02 -0300

    Configurando SignalR

--- MiniSignalServer/Program.cs
+++ MiniSignalServer/Program.cs
@@ -1,6 +1,16 @@
+using Microsoft.AspNetCore.SignalR;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSignalR();
+
 var app = builder.Build();
+app.MapHub<MyHub>("/chat");
 
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
+
+class MyHub : Hub
+{
+
+}
